77e0ecbb25523a724a5d91d9a9412f74e52bc5ab
mtoanng <nguyendungmanhtoan@gmail.com>
2026-07-18 15:38:09 +0700

    fix entry point, packaging, and Java-Clickhouse schema

--- flink-jobs/f1-telemetry-job/src/main/java/com/f1telemetry/InspectPOJOConvertor.java
+++ flink-jobs/f1-telemetry-job/src/main/java/com/f1telemetry/InspectPOJOConvertor.java
@@ -4,39 +4,8 @@ import java.lang.reflect.*;
 
 public class InspectPOJOConvertor {
     public static void main(String[] args) {
-        inspectClass("com.f1telemetry.avro.CarTelemetryEvent");
-        inspectClass("com.f1telemetry.model.TelemetryRollup");
-        inspectClass("org.apache.flink.connector.clickhouse.sink.ClickHouseAsyncSink");
-        inspectClass("org.apache.flink.connector.clickhouse.sink.ClickHouseAsyncSinkBuilder");
-        inspectClass("org.apache.flink.connector.clickhouse.sink.ClickHouseClientConfig");
-        
-        // Let's try to load common convertor classes to see which ones exist.
-        String[] possibleConvertors = {
-            "org.apache.flink.connector.clickhouse.convertor.POJOConvertor",
-            "org.apache.flink.connector.clickhouse.convertor.ClickHouseConvertor",
-            "org.apache.flink.connector.clickhouse.convertor.ClickHouseRowConverter",
-            "org.apache.flink.connector.clickhouse.internal.convertor.ClickHouseRowConverter"
-        };
-        for (String c : possibleConvertors) {
-            try {
-                Class<?> clazz = Class.forName(c);
-                System.out.println("\nFound Convertor Class: " + clazz.getName());
-                for (Constructor<?> constructor : clazz.getDeclaredConstructors()) {
-                    System.out.println("  Constructor: " + constructor);
-                }
-                for (Method m : clazz.getDeclaredMethods()) {
-                    System.out.println("  Method: " + m);
-                }
-            } catch (ClassNotFoundException e) {
-                System.out.println("Not found: " + c);
-            }
-        }
-    }
-
-    private static void inspectClass(String className) {
         try {
-            Class<?> clazz = Class.forName(className);
-            System.out.println("\n========================================");
+            Class<?> clazz = Class.forName("org.apache.flink.connector.clickhouse.sink.ClickHouseAsyncSink");
             System.out.println("Class: " + clazz.getName());
             System.out.println("Modifiers: " + Modifier.toString(clazz.getModifiers()));
             
@@ -45,17 +14,12 @@ public class InspectPOJOConvertor {
                 System.out.println("  " + c.toString());
             }
             
-            System.out.println("\nFields:");
-            for (Field f : clazz.getDeclaredFields()) {
-                System.out.println("  " + f.toString());
-            }
-            
             System.out.println("\nMethods:");
-            for (Method m : clazz.getMethods()) {
+            for (Method m : clazz.getDeclaredMethods()) {
                 System.out.println("  " + m.toString());
             }
         } catch (Exception e) {
-            System.err.println("Failed to inspect " + className + ": " + e.getMessage());
+            e.printStackTrace();
         }
     }
 }
